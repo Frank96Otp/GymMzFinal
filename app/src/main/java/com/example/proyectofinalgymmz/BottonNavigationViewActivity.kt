1a35b21ae5dfef82471a76f00f133270e6815d0d
Frank <frankpicis96l@gmail>
2023-08-13 09:40:28 -0500

    Bottom navigation view con fragments terminado

--- app/src/main/java/com/example/proyectofinalgymmz/BottonNavigationViewActivity.kt
+++ app/src/main/java/com/example/proyectofinalgymmz/BottonNavigationViewActivity.kt
@@ -2,10 +2,25 @@ package com.example.proyectofinalgymmz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.proyectofinalgymmz.databinding.ActivityBottonNavigationViewBinding
 
 class BottonNavigationViewActivity : AppCompatActivity() {
+
+    private lateinit var  binding: ActivityBottonNavigationViewBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_botton_navigation_view)
+        binding = ActivityBottonNavigationViewBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //configurando el fragment conteiner view
+        val fragmentView = supportFragmentManager.findFragmentById(R.id.fcv_navigation)as NavHostFragment
+        val navController = fragmentView.navController
+
+        binding.bnvMain.setupWithNavController(navController)
+
+
     }
 }
